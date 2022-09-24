006161c1246baa662179316c9961a4281457edae
azariahpundari1 <thenightman202@gmail.com>
2022-09-25 02:05:12 +1000

    Refactor password program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,27 @@
-password = input("Password: ")
-MIN_LENGTH = 10
-while len(password) < MIN_LENGTH:
-    print("Weak password")
+"""Module docstring"""
+
+
+# imports
+# CONSTANTS
+
+def main():
+    """Function docstring"""
     password = input("Password: ")
-for letter in password:
-    print('*', end='')
+    MIN_LENGTH = 10
+    password = get_password(MIN_LENGTH, password)
+    get_stars(password)
+
+
+def get_stars(password):
+    for i in password:
+        print('*', end='')
+
+
+def get_password(MIN_LENGTH, password):
+    while len(password) < MIN_LENGTH:
+        print("Weak password")
+        password = input("Password: ")
+    return password
+
+
+main()
