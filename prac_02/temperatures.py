7904565b21e8e17b1876e1ee192766c90ccc50eb
azariahpundari1 <thenightman202@gmail.com>
2022-09-25 02:17:23 +1000

    Removed print statement in functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,43 +1,41 @@
 """
-CP1404/CP5632 - Practical 1
+CP1404/CP5632 - Practical 2
 A program that converts Celsius to Fahrenheit and vise versa.
 Pseudocode for temperature conversion
-display menu
-get choice
-while choice != Q
-    if choice  = C
-        get celsius
-        fahrenheit = celsius * 9.0 / 5 = 32
-        display result
-    else if choice = F
-        get fahrenheit
-        celsius = 5 / 9 * (fahrenheit - 32)
-        display result
-    else
-        display invalid option
-    display menu
-    get choice
-display thank you
+
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-    else:
-        print("Invalid option")
+
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            get_fahrenheit(celsius)
+            print(get_fahrenheit(celsius))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            get_celsius(fahrenheit)
+            print(get_celsius(fahrenheit))
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            # Remove the "pass" statement when you are done. It's a placeholder.
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def get_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return f"Result: {celsius:.2f} C"
+
+
+def get_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return f"Result: {fahrenheit:.2f} C"
+
+
+main()
