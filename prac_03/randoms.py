109a0cabf54b014b74cf1481533ea6329632f7c8
azariahpundari1 <thenightman202@gmail.com>
2022-09-29 16:16:50 +1000

    Add answers to questions, add random number

---
+++ prac_03/randoms.py
@@ -0,0 +1,22 @@
+"""
+CP1404 - Practical 3
+"""
+
+import random
+# 1
+# 8, 5, 16, 8, 5, 20
+# Smallest number: 5
+# Largest number: 20
+
+# 2
+# 7, 3, 9, 7, 5, 3, 3
+# Smallest number: 3
+# Could it have produced a 4? No
+
+# 3
+# 4.915034498088989, 3.6862814650918603, 2.5114686119650838, 4.445934935520788
+# Smallest number: 2.5
+# Largest number: 5.5
+
+random_number = random.randint(0, 100)
+print(f"Here's a random number: {random_number}")
