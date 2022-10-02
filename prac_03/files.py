2517e4008f00aed8868e08089652272ef0a1670b
azariahpundari1 <thenightman202@gmail.com>
2022-10-02 19:18:34 +1000

    Add question 1

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,1 +1,14 @@
+"""
+CP1404/CP5632 - Practical 3
+To give experience in using different ways to read files
+"""
+
+# 1. A code that asks a user for the users input then opens a file called 'name.txt.' and writes their
+# name in it
+
+FILE_NAME = 'name.txt'
+name = input("Name: ").title()
+out_file = open(FILE_NAME, 'w')
+print(name, file=out_file)
+out_file.close()
 
