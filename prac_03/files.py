e93376e20bd7d1f1bdab84c66027dded1ccf6270
azariahpundari1 <thenightman202@gmail.com>
2022-10-02 19:59:07 +1000

    Add question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,13 +7,23 @@ To give experience in using different ways to read files
 # name in it
 
 FILE_NAME = 'name.txt'
-name = input("Name: ").title()
-out_file = open(FILE_NAME, 'w')
-print(name, file=out_file)
-out_file.close()
+# name = input("Name: ").title()
+# out_file = open(FILE_NAME, 'w')
+# print(name, file=out_file)
+# out_file.close()
 
 # 2. A code that opens 'name.txt' and reads the name and prints the name
 in_file = open(FILE_NAME, 'r')
 read_name = in_file.readline()
 print(f"Your name is {read_name}")
+in_file.close()
 
+# 3. Number stuff
+# for the sake of the code, made the variable a different name instead of "in_file"
+total = 0
+lines = []
+file_object = open("numbers.txt", 'r')
+for line in file_object:
+    lines.append(line.strip())
+print(int(lines[1]) + int(lines[0]))
+file_object.close()
