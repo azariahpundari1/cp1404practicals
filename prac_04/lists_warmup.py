0afcb97bd3ea4c86ee33c4507685e426c1d275a5
azariahpundari1 <thenightman202@gmail.com>
2022-10-05 17:59:40 +1000

    Add question, add answer

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,32 @@
+"""
+CP1404 Programming II warmup
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+print(numbers[0])
+# will print the first element
+
+print(numbers[-1])
+# will print the last element
+
+print(numbers[3])
+# will print the 4th element
+
+print(numbers[:-1])
+# will print all element except last element
+
+print(numbers[3:4])
+# will print in between elements 2 and 3
+
+print(5 in numbers)
+# will print True
+
+print(7 in numbers)
+# will print False
+
+print("3" in numbers)
+# will print False
+
+print(numbers + [6, 5, 3])
+# will append these numbers at the end of the list ?
