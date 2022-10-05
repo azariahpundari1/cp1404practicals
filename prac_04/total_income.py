6121dd6379ad18045d9eea7edb6ee56712c3142c
azariahpundari1 <thenightman202@gmail.com>
2022-10-05 18:09:00 +1000

    Refactor months - number_of_months

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,18 +7,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many number_of_months? "))
 
     # get income and stores in incomes
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        # income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
     total = 0
 
     # display stored income, calculates total income with operator
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
