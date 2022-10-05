310f0c204488be2efe82a523f35cfd276cbe7c18
azariahpundari1 <thenightman202@gmail.com>
2022-10-06 00:47:37 +1000

    Add print function, comment out unnecessary code

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,21 +8,35 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    # print(data)
+    print_subject_details(data)
+
+
+def print_subject_details(data):
+    for i in data:
+        subject = i[0]
+        lecturer = i[1]
+        number_students = i[2]
+        print(f"{subject} is taught by {lecturer} and has {number_students} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    nested_lists = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        nested_lists.append(parts)  # Puts list in a list (LISTCEPTION!)
+        # print(nested_lists)
         print("----------")
+        # print(nested_lists)
+    return nested_lists
     input_file.close()
 
 
