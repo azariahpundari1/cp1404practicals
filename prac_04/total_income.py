a7c5de0cacda47b82cbec7ff0f037f2a9e86fe07
azariahpundari1 <thenightman202@gmail.com>
2022-10-05 18:05:31 +1000

    Add comments

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -9,12 +9,15 @@ def main():
     incomes = []
     months = int(input("How many months? "))
 
+    # get income and stores in incomes
     for month in range(1, months + 1):
         income = float(input("Enter income for month " + str(month) + ": "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
     total = 0
+
+    # display stored income, calculates total income with operator
     for month in range(1, months + 1):
         income = incomes[month - 1]
         total += income
