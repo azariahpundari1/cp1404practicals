c213b79aa31d086f6434f5fa66dbd279302022b6
azariahpundari1 <thenightman202@gmail.com>
2022-10-25 00:11:16 +1000

    Add complete program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,22 +6,36 @@ Actual: my soul
 """
 
 email_to_name = {}
-email = input("Email: ")
-while email != '':
-    split_name = email.split('@')
-    # print(split_email)  # test
-    name = split_name[0]
-    email_to_name[email] = name  # key = recipients email
-    verify_name = input(f"Is your name {name}? [Y/n] ").upper()
-    if verify_name == 'Y' or verify_name == '':
-        email_to_name[email] = name
-    else:
-        new_name = input("Name: ").title()
-        email_to_name[email] = new_name
-    # print(email_to_name)
-    # name = "".join(email_to_name.keys()).title()
-    # verify_name = input(f"is your name {name}? [Y/n] ").upper()
-    email = input("Email: ")
 
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+
+def main():
+    """Initiate program"""
+    email = input("Email: ")
+    while email != '':
+        split_email = email.split('@')
+        # print(split_email)  # test
+        full_name = split_email[0]
+        split_name = full_name.split('.')
+        name = " ".join(split_name).title()
+        # print(name)  # test
+        set_name(email, name)
+        verify_name = input(f"Is your name {name}? [Y/n] ").upper()
+        if verify_name == 'Y' or verify_name == '':
+            # keeps value as current name
+            set_name(email, name)
+        else:
+            # sets value as new name
+            new_name = input("Name: ").title()
+            set_name(email, new_name)
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name.title()} ({email})")
+
+
+def set_name(email, name):
+    """Function that sets the name for the dictionary"""
+    email_to_name[email] = name  # key = recipients email
+
+
+main()
