e31eb58142bc1dac50bb50d20796a646a55a4f1f
azariahpundari1 <thenightman202@gmail.com>
2022-11-02 23:44:17 +1000

    Add complete program

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,18 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+    print(f"My guitar: {name}, first made in {year}")
+
+    guitar = Guitar(name, year, cost)
+    print(guitar)
+
+    print(f"{guitar.name} get_age() - Expected {50}. Got {guitar.get_age()}")
+    print()
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+
+
+main()
