587ba1ba65c0addf348fe89c6ade5cdca9ccaf98
azariahpundari1 <thenightman202@gmail.com>
2022-11-02 23:39:32 +1000

    Add complete programg

---
+++ prac_06/guitar.py
@@ -0,0 +1,20 @@
+CURRENT_YEAR = 2022
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : {self.cost}"
+
+    def get_age(self):
+        age = CURRENT_YEAR - self.year
+        return age
+
+    def is_vintage(self):
+        return self.get_age() >= 50
+
+
