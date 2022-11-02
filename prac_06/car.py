8f71574e2811584ee8ac6a8851fcfdd8f5e02484
azariahpundari1 <thenightman202@gmail.com>
2022-11-02 22:15:40 +1000

    Add program from GitHub

--- prac_06/car.py
+++ prac_06/car.py
@@ -5,27 +5,28 @@ class Car:
     """Represent a Car object."""
 
     def __init__(self, fuel=0):
-        """Initialise car instance.
+        """Initialise a Car instance.
 
-        fuel: float, one unit of fuel drives one kilometer
+        fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
-        self.odometer = 0
+        self._odometer = 0
 
-        def add_fuel(self, amount):
-            """Add amount to the car's fuel."""
-            self.fuel += amount
 
-        def drive(self, distance):
-            """Drive the car a given distance.
+    def add_fuel(self, amount):
+        """Add amount to the car's fuel."""
+        self.fuel += amount
 
-            Drive given distance if car has enough fuel
-            or drive until fuel runs out return the distance actually driven.
-            """
-            if distance > self.fuel:
-                distance = self.fuel
-                self.fuel = 0
-            else:
-                self.fuel -= distance
-            self.odometer += distance
-            return distance
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self._odometer += distance
+        return distance
