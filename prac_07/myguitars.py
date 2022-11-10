4e874d43aceba8ab05c9b68c64f036f47d04df86
azariahpundari1 <thenightman202@gmail.com>
2022-11-10 16:44:22 +1000

    Add function - load_file()

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,13 +6,43 @@ Guitars
 from prac_06.guitar import Guitar
 
 file_name = 'guitars.csv'
+MENU = '(A)dd guitar\n(D)isplay guitar\n(Q)uit'
 
 
 def main():
     """Read guitars from guitars.csv, store in list, display"""
     guitars = []
+    new_guitar = []
+    load_file(guitars)
+    # Sort guitars from oldest - newest
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+    print(f"You have {len(guitars)} guitars in your collection")
+
+    # going to make this similar to assessment01 because without a menu, its confusing
+    print(MENU)
+    option = input(">>> ").upper()
+    while option != 'Q':
+        if option == 'D':
+            for guitar in guitars:
+                print(guitar)
+        elif option == 'A':
+            name = input("Name: ").title()
+            year = int(input("Year: "))
+            price = float(input("Price: "))
+            guitar_to_append = append_guitar(name, new_guitar, price, year)
+            guitars.append(guitar_to_append)
+        else:
+            print("Invalid input")
+            print(MENU)
+        option = input(">>> ").upper()
+    print("Fin")
+    save_guitar(file_name, guitars)
+
+
+def load_file(guitars):
     in_file = open(file_name, 'r')
-    in_file.readline()  # skips first line format/protocol
     for line in in_file:
         parts = line.strip().split(',')
         # print(parts)
@@ -21,10 +51,13 @@ def main():
         guitars.append(guitar)
     in_file.close()
 
-    # Sort guitars from oldest - newest
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+
+def append_guitar(name, new_guitar, price, year):
+    """Append new guitar into list - new_guitar"""
+    new_guitar.append(name)
+    new_guitar.append(year)
+    new_guitar.append(price)
+    return identify_guitar_parts(new_guitar)
 
 
 def identify_guitar_parts(parts):
@@ -32,8 +65,16 @@ def identify_guitar_parts(parts):
     name = parts[0]
     year = parts[1]
     price = parts[2]
-    guitar = Guitar(name, year, price)
+    guitar = Guitar(name, str(year), str(price))
     return guitar
+
+
+def save_guitar(file_name, guitars):
+    out_file = open(file_name, 'w')
+    for i, guitar in enumerate(guitars):
+        str_guitar = [str(guitar) for guitar in guitar]
+        print(",".join(str_guitar), file=out_file)
+    out_file.close()
 
 
 main()
