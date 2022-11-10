f0dd767f07d624459d330716ae4213cb3eb98e78
azariahpundari1 <thenightman202@gmail.com>
2022-11-10 20:09:59 +1000

    Add __init__, add __str__

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,3 +4,20 @@ Project
 Estimate: 1hr
 Actual:
 """
+
+
+class ProjectManagement:
+    """A class representing project management object"""
+
+    def __init__(self, name="", date="", priority=0, cost=0, completion=0):
+        """Initiate program taking in project name, date, cost and completion in percenatage"""
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __str__(self):
+        """Return string showing project details"""
+        return f"{self.name}, start: {self.date}, priority: {self.priority}, estimate, completion: {self.completion}"
+
