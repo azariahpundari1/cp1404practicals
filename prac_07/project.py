e2bc30b39f9d0aabf0f4d540df478e9c8ae9a614
azariahpundari1 <thenightman202@gmail.com>
2022-11-10 20:10:13 +1000

    Add load_project function

--- prac_07/project.py
+++ prac_07/project.py
@@ -11,12 +11,14 @@ MENU = '(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects 
 
 FILE_NAME = 'projects.txt'
 
+
 def main():
     print(MENU)
     option = input(">>> ").upper()
     while option != 'Q':
         if option == 'L':
-            in_file = open(FILE_NAME, 'r')
+            projects = []
+            load_project(projects)
         elif option == 'S':
             pass
         elif option == 'D':
@@ -31,6 +33,20 @@ def main():
             print("Invalid input")
         print(MENU)
         option = input(">>> ").upper()
+    print("Thank you for using custom-built project management software.")
+
+
+def load_project(projects):
+    """Load project from user input"""
+    # file_name = input("File name followed by .txt, .csv etc: ")
+    in_file = open() # to make tests run quicker
+    in_file.readline()
+    for line in in_file:
+        # print(line)
+        parts = line.strip().split('  ')
+        projects.append(parts)
+    print(projects)
+    in_file.close()
 
 
 main()
