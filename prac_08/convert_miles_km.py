88d8828945e4aa1e382265eea1ae16313c08107e
azariahpundari1 <thenightman202@gmail.com>
2022-11-16 21:16:31 +1000

    Add basic features

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,23 @@
+"""
+CP1404 Programming II - Convert Miles to Kilometers
+A program that converts miles to kilometers
+By Azariah Pundari
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class ConvertMileKm(App):
+    """Convert mile to kilometer app start"""
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (350, 200)
+        self.title = "Square Number 2"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+ConvertMileKm().run()
