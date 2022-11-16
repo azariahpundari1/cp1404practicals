979998a70007f29febcf2ea6b92e30a8f5ba888d
azariahpundari1 <thenightman202@gmail.com>
2022-11-16 18:00:10 +1000

    Add function - handle_clear(), add function docstring

--- prac_08/KivyDemos-master/box_layout_demo.py
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -4,13 +4,19 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build a box layout model of greeting a user"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Greet the user after user enter name"""
         print('greet')  # test
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """Reset output label to default - Enter name"""
+        self.root.ids.output_label.text = "Enter name"
 
 
 BoxLayoutDemo().run()
