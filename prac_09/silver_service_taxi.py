ccec4d133644058419432ec53c013fb691451321
azariahpundari1 <thenightman202@gmail.com>
2022-11-18 18:11:16 +1000

    Add __str__ constructor

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Programming II Practical 9
+Silver Service Taxi class
+"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Represent silver service taxi object derived from taxi"""
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness=0.0):
+        """Initialize a silver service taxi instance"""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= self.fanciness
+
+    def __str__(self):
+        """Return a string displaying name, fuel, odo, distance, price per km, flagfall"""
+        return f"{self.name}, fuel={self.fuel}, odo={self.odometer}, {self.current_fare_distance}, {self.price_per_km} " \
+               f"plus flagfall {self.flagfall}"
+
+    def get_fare(self):
+        """Return price for silver service trip"""
