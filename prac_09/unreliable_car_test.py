c01cf141b2ea8115191a0d897fbc0e20b1d62eb7
azariahpundari1 <thenightman202@gmail.com>
2022-11-18 12:19:55 +1000

    Refactor to consist of reliable car & unreliable car

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -7,14 +7,22 @@ from prac_09.unreliable_car import UnreliableCar
 
 def main():
     """Test to see if unreliable_car.py works"""
-    car = UnreliableCar("Car", 100, 100)
-    print(car)
-    car.drive(20)
-    print(car)
-    car.drive(20)
-    print(car)
-    car.drive(10)
-    print(car)
+    reliable_car = UnreliableCar("Reliable Car", 100, 100)
+    unreliable_car = UnreliableCar("Unreliable Car", 100, 10)
+    print(reliable_car)
+    print(unreliable_car)
+    reliable_car.drive(20)
+    unreliable_car.drive(20)
+    print(reliable_car)
+    print(unreliable_car)
+    reliable_car.drive(20)
+    unreliable_car.drive(20)
+    print(reliable_car)
+    print(unreliable_car)
+    reliable_car.drive(10)
+    unreliable_car.drive(10)
+    print(reliable_car)
+    print(unreliable_car)
 
 
 main()
