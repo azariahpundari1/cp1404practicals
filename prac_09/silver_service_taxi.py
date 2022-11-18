d95ff854c1629d592e0b99c1ede6f2b6464eab2a
azariahpundari1 <thenightman202@gmail.com>
2022-11-18 18:48:58 +1000

    Add test code for quickies

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -18,9 +18,18 @@ class SilverServiceTaxi(Taxi):
 
     def __str__(self):
         """Return a string displaying name, fuel, odo, distance, price per km, flagfall"""
-        return f"{self.name}, fuel={self.fuel}, odo={self.odometer}, {self.current_fare_distance}, " \
-               f"${self.price_per_km * self.flagfall}/km plus flagfall of {self.flagfall}"
+        return f"{self.name}, fuel={self.fuel}, odo={self.odometer}, {self.current_fare_distance}km on current fair, " \
+               f"${self.price_per_km}/km plus flagfall of {self.flagfall}"
 
     def get_fare(self):
         """Return price for silver service trip"""
-        return (self.price_per_km * self.flagfall) * self.current_fare_distance
+        return self.flagfall * self.fanciness
+
+
+if __name__ == '__main__':
+    test = SilverServiceTaxi("Hammer", 200, 4)
+    print(test)
+    test.drive(20)
+    print(test)
+    test.start_fare()
+    print(test)
