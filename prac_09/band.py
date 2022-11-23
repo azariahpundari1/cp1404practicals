00eeb99f42e976e0017c791bc7ebcc221e3a4e7f
azariahpundari1 <thenightman202@gmail.com>
2022-11-23 22:12:21 +1000

    Add components to make my_band.py work

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,1 +1,14 @@
 """Band class for CP1404"""
+
+
+class Band:
+    """Band class"""
+
+    def __init__(self, band_name=""):
+        pass
+
+    def add(self, object):
+        pass
+
+    def play(self):
+        pass
