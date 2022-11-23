6c49c44d0fe21ed84cc9ef42ef8bdcd70bab6f42
azariahpundari1 <thenightman202@gmail.com>
2022-11-23 17:45:59 +1000

    Add features to make my_band.py work not properly

---
+++ prac_09/band.py
@@ -0,0 +1,17 @@
+"""Band class for CP1404"""
+
+
+class Band:
+    """Band class"""
+
+    def __init__(self, name=""):
+        self.name = name
+
+    def __str__(self):
+        return f"print"
+
+    def add(self, nuno):
+        pass
+
+    def play(self):
+        pass
