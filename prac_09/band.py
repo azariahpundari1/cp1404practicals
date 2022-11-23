38427e7d8eaea0d7493de501d8c037f59f592572
azariahpundari1 <thenightman202@gmail.com>
2022-11-23 18:26:18 +1000

    Add __repr__, Add add() function

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,17 +1,25 @@
 """Band class for CP1404"""
+from musician import Musician
 
 
 class Band:
     """Band class"""
 
     def __init__(self, band_name=""):
+        """Construct a Band with a band name and an empty musicians collection"""
         self.band_name = band_name
+        self.musicians = []
 
     def __str__(self):
-        return f"print"
+        print("Band string")
+        return f"{self.band_name} ({self.musicians[4]})"
 
-    def add(self, nuno):
-        pass
+    def __repr__(self):
+        print("Repr string")
+        return str(vars(self))
+
+    def add(self, musician):
+        self.musicians.append(musician)
 
     def play(self):
         pass
