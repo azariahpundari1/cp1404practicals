b5efe92e50024a8f3a7cc7b4a729e826168bb251
azariahpundari1 <thenightman202@gmail.com>
2022-11-30 22:01:12 +1000

    Reset program

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -7,11 +7,6 @@ from wikipedia import wikipedia
 def main():
     """Main script that asks user for a page title or phrase"""
     user_input = input("Enter phrase or title: ")
-    while user_input != '':
-        wiki_output = wikipedia.summary(user_input, 3, 350)
-        print(wiki_output)
-        user_input = input("Enter phrase or title: ")
-    print("Finished.")
 
 
 main()
