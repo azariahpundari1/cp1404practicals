52efde4dae1cd7862486f97bde2e647a9635d398
azariahpundari1 <thenightman202@gmail.com>
2022-11-30 22:59:53 +1000

    Refactor title string

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -11,7 +11,7 @@ def main():
     while user_input != '':
         # pycharm suggested to add this to avoid BeautifulSoup package
         wiki_title = wikipedia.search(user_input, 1)
-        wiki_title_str = "".join(wiki_title)
+        wiki_title_str = " ".join(wiki_title)
         wiki_summary = wikipedia.summary(user_input)
         print(f"{wiki_title_str}\n{wiki_summary}")
 
